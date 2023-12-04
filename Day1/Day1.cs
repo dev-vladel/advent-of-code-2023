51168b88e2144625de80c50112de40916bf6baa4
Vladel R9xCKET <vladghe@gmail.com>
2023-12-04 23:50:54 +0200

    day 1 folder. updated gitignore

---
+++ Day1/Day1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace advent_of_code_2023.Day1
+{
+    internal class Day1
+    {
+    }
+}
