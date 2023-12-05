45c76e2d3f1d68c227c85f8fb01ce334ca13dad7
Vladel R9xCKET <vladghe@gmail.com>
2023-12-05 23:16:38 +0200

    feature: day 4 part 1

--- Day4/Day4.cs
+++ Day4/Day4.cs
@@ -8,6 +8,27 @@
             var input = File.ReadAllLines("../../../Day4/input.txt");
 
             //solution
+            var winningNumbers = new List<int>();
+            var availableNumbers = new List<int>();
+
+            foreach (var line in input)
+            {
+                var points = 0;
+
+                var split = line.Split('|');
+                winningNumbers = split[0].Split(':')[1].Split(' ').Where(s => !string.IsNullOrEmpty(s)).Select(int.Parse).ToList();
+                availableNumbers = split[1].Split(' ').Where(s => !string.IsNullOrEmpty(s)).Select(int.Parse).ToList();
+
+                var matchingNumbers = availableNumbers.Intersect(winningNumbers).ToList();
+
+                foreach (var match in matchingNumbers)
+                {
+                    points = points == 0 ? 1 : points * 2;
+                }
+
+                output += points;
+            }
+
 
             Console.WriteLine($"Result of Day 4 - Part 1 is {output}");
         }
@@ -15,7 +36,7 @@
         public static void SolvePart2()
         {
             var output = 0;
-            var input = File.ReadAllLines("../../../Day4/input.txt");
+            var input = File.ReadAllLines("../../../Day4/testInput.txt");
 
             //solution
 
