e34a150a8562bf449376d9847b3341e0d4f5de06
Vladel R9xCKET <vladghe@gmail.com>
2023-12-05 04:54:42 +0200

    feature: day 2 part 1

---
+++ Day2/Day2.cs
@@ -0,0 +1,63 @@
+﻿namespace advent_of_code_2023.Day2
+{
+    public class Day2
+    {
+        public static void SolvePart1()
+        {
+            var output = 0;
+            var input = File.ReadAllLines("../../../Day2/input.txt");
+
+            foreach (var line in input)
+            {
+                var lineSplit  = line.Split(':').Select(s => s.Trim()).ToList();
+                var gameId = lineSplit[0].Split(" ")[1];
+                var cubeHands = lineSplit[1].Split(";").Select(s => s.Trim()).ToList();
+                var possibleGame = true;
+
+                foreach (var hand in cubeHands)
+                {
+                    var pairs = hand.Contains(",") ? hand.Split(",").Select(hand => hand.Trim()) : new[] { hand };
+
+                    foreach (var pair in pairs)
+                    {
+                        var detail = pair.Split(" ");
+                        var number = int.Parse(detail[0]);
+                        var color = detail[1];
+
+                        switch (color)
+                        {
+                            case "red":
+                                possibleGame = possibleGame && number <= 12;
+                                break;
+
+                            case "blue":
+                                possibleGame = possibleGame && number <= 14;
+                                break;
+
+                            case "green":
+                                possibleGame = possibleGame && number <= 13;
+                                break;
+                        }
+                    }
+                }
+
+                if (possibleGame)
+                {
+                    output += int.Parse(gameId);
+                }
+            }
+
+            Console.WriteLine($"Result of Day 1 - Part 1 is {output}");
+
+        }
+
+        public static void SolvePart2()
+        {
+            var output = 0;
+            var input = File.ReadAllLines("../../../Day2/input.txt");
+
+            Console.WriteLine($"Result of Day 1 - Part 1 is {output}");
+            Console.WriteLine("---");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
 ﻿using advent_of_code_2023.Day1;
+using advent_of_code_2023.Day2;
 
 Day1.SolvePart1();
 Day1.SolvePart2();
+
+Day2.SolvePart1();
+Day2.SolvePart2();
