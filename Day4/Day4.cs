ae57cd125e0eb947a22e44ad9c827d8647ed9be6
Vladel R9xCKET <vladghe@gmail.com>
2023-12-06 00:42:31 +0200

    feature: day 4 part 2

--- Day4/Day4.cs
+++ Day4/Day4.cs
@@ -39,8 +39,53 @@
             var input = File.ReadAllLines("../../../Day4/testInput.txt");
 
             //solution
+            List<Card> cards = new List<Card>();
+
+            foreach (var line in input)
+            {
+                var card = new Card();
+
+                var split = line.Split('|');
+
+                card.Id = int.Parse(split[0].Split(':')[0].Split(' ')[1]);
+                card.WinningNumbers = split[0].Split(':')[1].Split(' ').Where(s => !string.IsNullOrEmpty(s)).Select(int.Parse).ToList();
+                card.AvailableNumbers = split[1].Split(' ').Where(s => !string.IsNullOrEmpty(s)).Select(int.Parse).ToList();
+
+                cards.Add(card);
+            }
+
+            var numberOfCards = new int[cards.Count];
+
+            for (var i = 0; i < cards.Count; i++)
+            {
+                var card = cards[i];
+                numberOfCards[i] += 1;
+
+                var matchingNumbers = card.WinningNumbers.Intersect(card.AvailableNumbers).Count();
+
+                if (matchingNumbers > 0)
+                {
+                    for (int j = 0, idToIncrease = i + 1; j < matchingNumbers && idToIncrease <= cards.Count; j++)
+                    {
+                        numberOfCards[idToIncrease++] += numberOfCards[i];
+                    }
+                }
+            }
+
+            foreach (var number in numberOfCards)
+            {
+                output += number;
+            }
 
             Console.WriteLine($"Result of Day 4 - Part 2 is {output}");
+        }
+
+        private class Card
+        {
+            public int Id { get; set; }
+            public List<int> WinningNumbers { get; set; } = new List<int>();
+            public List<int> AvailableNumbers { get; set; } = new List<int>();
+            public int NumberOfOccurences { get; set; } = 0;
         }
     }
 }
