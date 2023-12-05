ee9c606db870207cdfc083034aa95ba78f3bbbe5
Vladel R9xCKET <vladghe@gmail.com>
2023-12-06 00:57:36 +0200

    feature: day 5 base

---
+++ Day5/Day5.cs
@@ -0,0 +1,27 @@
+﻿namespace advent_of_code_2023.Day5
+{
+    public class Day5
+    {
+        public static void SolvePart1()
+        {
+            var output = 0;
+            //var input = File.ReadAllLines("../../../Day5/input.txt");
+            var input = File.ReadAllLines("../../../Day5/testInput.txt");
+
+            //solution
+
+            Console.WriteLine($"Result of Day 5 - Part 1 is {output}");
+        }
+
+        public static void SolvePart2()
+        {
+            var output = 0;
+            //var input = File.ReadAllLines("../../../Day5/input.txt");
+            var input = File.ReadAllLines("../../../Day5/testInput.txt");
+
+            //solution
+
+            Console.WriteLine($"Result of Day 5 - Part 2 is {output}");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using advent_of_code_2023.Day2;
 using advent_of_code_2023.Day3;
 using advent_of_code_2023.Day4;
+using advent_of_code_2023.Day5;
 using advent_of_code_2023.Day6;
 
 Day1.SolvePart1();
@@ -15,6 +16,9 @@ Day3.SolvePart2();
 
 Day4.SolvePart1();
 Day4.SolvePart2();
+
+Day5.SolvePart1();
+Day5.SolvePart2();
 
 Day6.SolvePart1();
 Day6.SolvePart2();
