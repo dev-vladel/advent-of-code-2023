a1f885185412b8f3e55b71a175d333322882af40
Vladel R9xCKET <vladghe@gmail.com>
2023-12-05 16:16:18 +0200

    feature: base day 3

---
+++ Day3/Day3.cs
@@ -0,0 +1,26 @@
+﻿namespace advent_of_code_2023.Day3
+{
+    public class Day3
+    {
+        public static void SolvePart1()
+        {
+            var output = 0;
+            var input = File.ReadAllLines("../../../Day2/input.txt");
+
+            // solution
+
+            Console.WriteLine($"Result of Day 1 - Part 1 is {output}");
+        }
+
+        public static void SolvePart2()
+        {
+            var output = 0;
+            var input = File.ReadAllLines("../../../Day2/input.txt");
+
+            // solution
+
+            Console.WriteLine($"Result of Day 1 - Part 1 is {output}");
+            Console.WriteLine("---");
+        }
+    }
+}
