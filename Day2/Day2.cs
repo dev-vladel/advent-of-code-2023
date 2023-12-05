935e6964ebe13a01c9e2a88eb30703c373a4a2d2
Vladel R9xCKET <vladghe@gmail.com>
2023-12-05 05:07:42 +0200

    feature: day 2 part 2

--- Day2/Day2.cs
+++ Day2/Day2.cs
@@ -9,7 +9,7 @@
 
             foreach (var line in input)
             {
-                var lineSplit  = line.Split(':').Select(s => s.Trim()).ToList();
+                var lineSplit = line.Split(':').Select(s => s.Trim()).ToList();
                 var gameId = lineSplit[0].Split(" ")[1];
                 var cubeHands = lineSplit[1].Split(";").Select(s => s.Trim()).ToList();
                 var possibleGame = true;
@@ -55,6 +55,46 @@
         {
             var output = 0;
             var input = File.ReadAllLines("../../../Day2/input.txt");
+
+            foreach (var line in input)
+            {
+                var lineSplit = line.Split(':').Select(s => s.Trim()).ToList();
+                var gameId = lineSplit[0].Split(" ")[1];
+                var cubeHands = lineSplit[1].Split(";").Select(s => s.Trim()).ToList();
+
+                var redMinimum = 0;
+                var blueMinimum = 0;
+                var greenMinimum = 0;
+
+                foreach (var hand in cubeHands)
+                {
+                    var pairs = hand.Contains(",") ? hand.Split(",").Select(hand => hand.Trim()) : new[] { hand };
+
+                    foreach (var pair in pairs)
+                    {
+                        var detail = pair.Split(" ");
+                        var number = int.Parse(detail[0]);
+                        var color = detail[1];
+
+                        switch (color)
+                        {
+                            case "red":
+                                redMinimum = redMinimum <= number ? number : redMinimum;
+                                break;
+
+                            case "blue":
+                                blueMinimum = blueMinimum <= number ? number : blueMinimum;
+                                break;
+
+                            case "green":
+                                greenMinimum = greenMinimum <= number ? number : greenMinimum;
+                                break;
+                        }
+                    }
+                }
+
+                output += redMinimum * greenMinimum * blueMinimum;
+            }
 
             Console.WriteLine($"Result of Day 1 - Part 1 is {output}");
             Console.WriteLine("---");
