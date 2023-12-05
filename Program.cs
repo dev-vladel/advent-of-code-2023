2a172947fe356fb78e28cc0b249b259444078216
Vladel R9xCKET <vladghe@gmail.com>
2023-12-06 00:57:36 +0200

    feature: day 5 base

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using advent_of_code_2023.Day2;
 using advent_of_code_2023.Day3;
 using advent_of_code_2023.Day4;
+using advent_of_code_2023.Day5;
 
 Day1.SolvePart1();
 Day1.SolvePart2();
@@ -14,3 +15,6 @@ Day3.SolvePart2();
 
 Day4.SolvePart1();
 Day4.SolvePart2();
+
+Day5.SolvePart1();
+Day5.SolvePart2();
