6539650477151bad30f92333c468a778edb33a21
Vladel R9xCKET <vladghe@gmail.com>
2023-12-05 22:59:33 +0200

    feature: day 4 base

---
+++ Day4/Day4.cs
@@ -0,0 +1,25 @@
+﻿namespace advent_of_code_2023.Day4
+{
+    public class Day4
+    {
+        public static void SolvePart1()
+        {
+            var output = 0;
+            var input = File.ReadAllLines("../../../Day4/input.txt");
+
+            //solution
+
+            Console.WriteLine($"Result of Day 4 - Part 1 is {output}");
+        }
+        
+        public static void SolvePart2()
+        {
+            var output = 0;
+            var input = File.ReadAllLines("../../../Day4/input.txt");
+
+            //solution
+
+            Console.WriteLine($"Result of Day 4 - Part 2 is {output}");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using advent_of_code_2023.Day1;
 using advent_of_code_2023.Day2;
 using advent_of_code_2023.Day3;
+using advent_of_code_2023.Day4;
 
 Day1.SolvePart1();
 Day1.SolvePart2();
@@ -10,3 +11,6 @@ Day2.SolvePart2();
 
 Day3.SolvePart1();
 Day3.SolvePart2();
+
+Day4.SolvePart1();
+Day4.SolvePart2();
