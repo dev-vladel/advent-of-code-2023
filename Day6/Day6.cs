e9109b2fff779e0afdb3050f8d6141a9898c2e4d
Vlad Gheorghiu <vlad.gheorghiu@pitechplus.onmicrosoft.com>
2023-12-06 13:08:17 +0200

    feature: day 6 part 2 quadratic and brute force

--- Day6/Day6.cs
+++ Day6/Day6.cs
@@ -6,16 +6,16 @@ namespace advent_of_code_2023.Day6
     {
         public static void SolvePart1()
         {
-            var outputQuadratic = 1;
-            var outputBruteForce = 1;
+            double outputQuadratic = 1;
+            double outputBruteForce = 1;
             var input = File.ReadAllLines("../../../Day6/input.txt");
 
             //solution
             var timeLine = input[0];
             var distanceLine = input[1];
 
-            var timeInfo = new TimeInfo() { Times = timeLine.Split(':')[1].Split(' ').Where(t => !string.IsNullOrEmpty(t)).Select(int.Parse).ToList() };
-            var distanceInfo = new DistanceInfo() { Distances = distanceLine.Split(':')[1].Split(' ').Where(t => !string.IsNullOrEmpty(t)).Select(int.Parse).ToList() };
+            var timeInfo = new TimeInfo() { Times = timeLine.Split(':')[1].Split(' ').Where(t => !string.IsNullOrEmpty(t)).Select(double.Parse).ToList() };
+            var distanceInfo = new DistanceInfo() { Distances = distanceLine.Split(':')[1].Split(' ').Where(t => !string.IsNullOrEmpty(t)).Select(double.Parse).ToList() };
 
             var pairs = timeInfo.Times.Zip(distanceInfo.Distances);
 
@@ -25,12 +25,12 @@ namespace advent_of_code_2023.Day6
                 var discriminant = Math.Sqrt(pair.First * pair.First - 4 * pair.Second);
                 var numberOfWays = Math.Floor((pair.First + discriminant) / 2) - Math.Ceiling((pair.First - discriminant) / 2) + 1;
 
-                outputQuadratic *= (int)numberOfWays;
+                outputQuadratic *= numberOfWays;
 
                 // Brute Force (works flawlessly)
                 var recordBeaten = 0;
 
-                for (int i = 0; i <= pair.First; i++)
+                for (long i = 0; i <= pair.First; i++)
                 {
                     var distanceTraveled = i * pair.First - i * i;
 
@@ -49,22 +49,50 @@ namespace advent_of_code_2023.Day6
 
         public static void SolvePart2()
         {
-            var output = 0;
-            var input = File.ReadAllLines("../../../Day6/testInput.txt");
+            double outputQuadratic = 1;
+            double outputBruteForce = 1;
+            var input = File.ReadAllLines("../../../Day6/input.txt");
 
             //solution
+            var timeLine = input[0];
+            var distanceLine = input[1];
 
-            Console.WriteLine($"Result of Day 6 - Part 2 is {output}");
+            var timeInfo = double.Parse(timeLine.Split(':')[1].Replace(" ", ""));
+            var distanceInfo = double.Parse(distanceLine.Split(':')[1].Replace(" ", ""));
+
+            // Quadratic (fails for example input)
+            var discriminant = Math.Sqrt(timeInfo * timeInfo - 4 * distanceInfo);
+            var numberOfWays = Math.Floor((timeInfo + discriminant) / 2) - Math.Ceiling((timeInfo - discriminant) / 2) + 1;
+
+            outputQuadratic *= numberOfWays;
+
+            // Brute Force (works flawlessly)
+            var recordBeaten = 0;
+
+            for (long i = 0; i <= timeInfo; i++)
+            {
+                var distanceTraveled = i * timeInfo - i * i;
+
+                if (distanceTraveled > distanceInfo)
+                {
+                    recordBeaten++;
+                }
+            }
+
+            outputBruteForce *= recordBeaten;
+
+            Console.WriteLine($"Result of Day 6 - Part 2 (quadratic) is {outputQuadratic}");
+            Console.WriteLine($"Result of Day 6 - Part 2 (brute force) is {outputBruteForce}");
         }
 
         private class TimeInfo
         {
-            public List<int> Times {  get; set; } = new List<int>();
+            public List<double> Times {  get; set; } = new List<double>();
         }
 
         private class DistanceInfo
         {
-            public List<int> Distances { get; set; } = new List<int>();
+            public List<double> Distances { get; set; } = new List<double>();
         }
     }
 }
