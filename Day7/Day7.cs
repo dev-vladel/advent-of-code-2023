ffc3d63f54c14d6e59ae1ce835f5b659e1976f50
Vlad Gheorghiu <vlad.gheorghiu@pitechplus.onmicrosoft.com>
2023-12-07 17:09:38 +0200

    feature: day 7 part 1

--- Day7/Day7.cs
+++ Day7/Day7.cs
@@ -4,12 +4,92 @@
     {
         public static void SolvePart1()
         {
-            int output = 0;
+            double output = 0;
             var input = File.ReadAllLines("../../../Day7/input.txt");
 
-            //solution
+            var hands = new List<Hand>();
 
-            Console.WriteLine($"Result of Day 7- Part 1 is {output}");
+            //solution
+            foreach (var line in input)
+            {
+                var split = line.Split(' ');
+
+                var hand = new Hand()
+                {
+                    Label = split[0],
+                    LabelTypes = split[0].Distinct().ToList(),
+                    Bid = int.Parse(split[1])
+                };
+
+                foreach (var character in hand.LabelTypes)
+                {
+                    var count = hand.Label.Count(l => l == character);
+                    hand.Strength[character.ToString()] = count;
+                }
+
+                var maxStrength = hand.Strength.Values.Max();
+
+                switch (maxStrength)
+                {
+                    // High Card
+                    case 1:
+                        hand.TypeOfPair = 0;
+                        break;
+
+                    // One pair or Two Pair
+                    case 2:
+                        hand.Strength.Remove(hand.Strength.FirstOrDefault(h => h.Value == maxStrength).Key);
+                        hand.TypeOfPair = hand.Strength.Values.Max() == 1 ? 1 : 2;
+
+                        break;
+
+                    // Three of a Kind or Full House
+                    case 3:
+                        hand.Strength.Remove(hand.Strength.FirstOrDefault(h => h.Value == maxStrength).Key);
+                        hand.TypeOfPair = hand.Strength.Values.Max() == 2 ? 4 : 3;
+
+                        break;
+
+                    // Four of a Kind
+                    case 4:
+                        hand.TypeOfPair = 5;
+                        break;
+
+                    // Five of a Kind
+                    case 5:
+                        hand.TypeOfPair = 6;
+                        break;
+                }
+
+                hands.Add(hand);
+            }
+
+            var orderedHand = new List<Hand>();
+
+            for (int i = 0; i <= 6; i++)
+            {
+                var pairList = hands.Where(h => h.TypeOfPair == i).ToList();
+
+                for (int j = 0; j < pairList.Count - 1; j++)
+                {
+                    for (int h = j + 1; h < pairList.Count; h++)
+                    {
+                        if (VerifyStrength(pairList[j], pairList[h]))
+                        {
+                            (pairList[h], pairList[j]) = (pairList[j], pairList[h]);
+                        }
+                    }
+                }
+
+                orderedHand.AddRange(pairList);
+            }
+
+            foreach (var hand in orderedHand)
+            {
+                output += hand.Bid * (orderedHand.IndexOf(hand) + 1);
+            }
+
+            Console.WriteLine($"Result of Day 7 - Part 1 is {output}");
         }
 
         public static void SolvePart2()
@@ -20,6 +100,52 @@
             //solution
 
             Console.WriteLine($"Result of Day 7 - Part 2 is {output}");
+        }
+
+        private class Hand
+        {
+            public int TypeOfPair { get; set; } = 0;
+            public List<char> LabelTypes { get; set; } = new List<char>();
+            public string Label { get; set; } = string.Empty;
+            public int Bid { get; set; } = 0;
+
+            public Dictionary<string, int> Strength = new()
+            {
+                {"2", 0},
+                {"3", 0},
+                {"4", 0},
+                {"5", 0},
+                {"6", 0},
+                {"7", 0},
+                {"8", 0},
+                {"9", 0},
+                {"T", 0},
+                {"J", 0},
+                {"Q", 0},
+                {"K", 0},
+                {"A", 0}
+            };
+        }
+
+        private static bool VerifyStrength(Hand one, Hand two)
+        {
+            var result = false;
+
+            for (int i = 0; i < one.Label.Length; i++)
+            {
+                if (one.Label[i] == two.Label[i])
+                {
+                    continue;
+                }
+                else
+                {
+                    result = Constants.CardValues[one.Label[i].ToString()] > Constants.CardValues[two.Label[i].ToString()];
+
+                    break;
+                }
+            }
+
+            return result;
         }
     }
 }
