0fb0a8b5c7de7174771c82de9beff490eb15b25f
Vladel R9xCKET <vladghe@gmail.com>
2023-12-08 14:15:39 +0200

    feature: day 8 base

---
+++ Day8/Day8.cs
@@ -0,0 +1,25 @@
+﻿namespace advent_of_code_2023.Day8
+{
+    public class Day8
+    {
+        public static void SolvePart1()
+        {
+            double output = 0;
+            var input = File.ReadAllLines("../../../Day8/testInput.txt");
+
+            // solution
+
+            Console.WriteLine($"Result of Day 8 - Part 1 is {output}");
+        }
+
+        public static void SolvePart2()
+        {
+            double output = 0;
+            var input = File.ReadAllLines("../../../Day8/testInput.txt");
+
+            // solution
+
+            Console.WriteLine($"Result of Day 8 - Part 2 is {output}");
+        }
+    }
+}
