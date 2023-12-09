8a31d27a6cf79762be7e303b2ff54f66dfd3dc88
Vlad Gheorghiu <vlad.gheorghiu@pitechplus.onmicrosoft.com>
2023-12-09 14:40:10 +0200

    feature: day 9 base

---
+++ Day9/Day9.cs
@@ -0,0 +1,25 @@
+﻿namespace advent_of_code_2023.Day9
+{
+    public static class Day9
+    {
+        public static void SolvePart1()
+        {
+            double output = 0;
+            var input = File.ReadAllLines("../../../Day9/input.txt");
+
+            // solution
+
+            Console.WriteLine($"Result of Day 9 - Part 1 is {output}");
+        }
+
+        public static void SolvePart2()
+        {
+            double output = 0;
+            var input = File.ReadAllLines("../../../Day9/input.txt");
+
+            // solution
+
+            Console.WriteLine($"Result of Day 9 - Part 2 is {output}");
+        }
+    }
+}
