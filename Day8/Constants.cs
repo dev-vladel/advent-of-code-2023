af6633faf9c2a9c4ebcff9b99e2a4d11454bcdb8
Vladel R9xCKET <vladghe@gmail.com>
2023-12-14 02:28:09 +0200

    feature: day 8 part 1

---
+++ Day8/Constants.cs
@@ -0,0 +1,11 @@
+﻿namespace advent_of_code_2023.Day8
+{
+    public static class Constants
+    {
+        public readonly static string StartingPosition = "AAA";
+        public readonly static string FinalPosition = "ZZZ";
+
+        public readonly static char InstructionLeft = 'L';
+        public readonly static char InstructionRight = 'R';
+    }
+}
--- Day8/Day8.cs
+++ Day8/Day8.cs
@@ -5,9 +5,40 @@
         public static void SolvePart1()
         {
             double output = 0;
-            var input = File.ReadAllLines("../../../Day8/testInput.txt");
+            var input = File.ReadAllLines("../../../Day8/input.txt");
 
-            // solution
+            var instructions = input[0];
+            var elements = input.Skip(2).Select(e =>
+            {
+                var split = e.Split("=")[1].Split(",");
+                var leftSide = split[0].Trim().Substring(1);
+                var rightSide = split[1].Trim().Substring(0, 3);
+
+                return (Key: e.Split("=")[0].Trim(), LeftSide: leftSide, RightSide: rightSide);
+            }).ToDictionary(d => d.Key, d => (d.Key, d.LeftSide, d.RightSide));
+
+            var instructionIdentifier = Constants.StartingPosition;
+            var element = elements[instructionIdentifier];
+
+            var finalPositionNotFound = true;
+
+            while (finalPositionNotFound)
+            {
+                foreach (var instruction in instructions)
+                {
+                    instructionIdentifier = instruction.Equals(Constants.InstructionLeft)
+                        ? element.LeftSide
+                        : element.RightSide;
+
+                    element = elements[instructionIdentifier];
+                    output++;
+
+                    if (element.Key.Equals(Constants.FinalPosition))
+                    {
+                        finalPositionNotFound = false;
+                    }
+                }
+            }
 
             Console.WriteLine($"Result of Day 8 - Part 1 is {output}");
         }
