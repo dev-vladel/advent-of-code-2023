194553ea81cf1ed2724f0b40ff3c83efcfc5ae5e
Vladel R9xCKET <vladghe@gmail.com>
2023-12-14 03:23:21 +0200

    feature: day 9 base

---
+++ Day9/Day9.cs
@@ -0,0 +1,27 @@
+﻿namespace advent_of_code_2023.Day9
+{
+    public class Day9
+    {
+        public static void SolvePart1()
+        {
+            double output = 0;
+            var input = File.ReadAllLines("../../../Day9/testInput.txt");
+
+            // solution
+
+
+            Console.WriteLine($"Result of Day 9 - Part 1 is {output}");
+        }
+
+        public static void SolvePart2()
+        {
+            double output = 0;
+            var input = File.ReadAllLines("../../../Day9/testInput.txt");
+
+            // solution
+
+
+            Console.WriteLine($"Result of Day 9 - Part 2 is {output}");
+        }
+    }
+}
